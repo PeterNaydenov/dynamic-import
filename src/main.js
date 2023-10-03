52da53205ce46a12d1e4a33c76ecb73d62a84800
Peter Naydenov <peter.naydenov@gmail.com>
2023-10-03 23:13:13 +0300

    jsDocs comments(types)

--- src/main.js
+++ src/main.js
@@ -1,5 +1,15 @@
+'use strict'
+/**
+ * @param {Function} importFn - 'import-pattern' function
+ * @returns {Function} requestList - function that expect list of modules
+ */
 function dynamicImport ( importFn ) {
-    return function requestList ( list ) {
+/**
+ * Function that expect list of modules
+ * @param {Array.<string|Object>} list - Data argument for 'import-pattern' function
+ * @returns {Promise} - promise that resolve to array of modules
+ */
+return function requestList ( list ) {
         const promises = [];
         list.forEach ( src =>  promises.push ( 
                                             importFn ( src )
