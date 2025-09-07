cf0793280717b0da969a92373171806e7065e299
Peter Naydenov <peter.naydenov@gmail.com>
2025-09-07 10:22:29 +0300

    Test: ImportPattern  with custom module name

--- test/01-general.cy.js
+++ test/01-general.cy.js
@@ -26,6 +26,29 @@ it ('general test', done => {
 
 
 
+it ( 'Load a custom module name', done => {
+      const importPatternFn = req => {
+                                    let
+                                          file
+                                        , name = 'default'
+                                        ;
+                                    if (req.includes ( ':') )    [ file, name ] = req.split ( ':' )
+                                    else                          file = req;
+                                    return import ( `./${file}-02.js` ).then ( module => module[name] )
+                              } // importPatternFn func.
+
+      let importEngine = dynamicImport ( importPatternFn );
+      importEngine ( [ 'test:other' ] )
+            .then ( ls  => {
+                      const fn = ls[0].callme;
+                      expect ( fn ).to.be.a ( 'function' )
+                      expect ( fn() ).to.be.equal ( 'Goodbye!' )
+                      done ()
+                  })
+}) // it Load a custom module name
+
+
+
 }) // describe
 
 
