730982e1d44ea7015dc1a489c36ab250f2532155
Peter Naydenov <peter.naydenov@gmail.com>
2026-09-09 06:22:56 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
   publicDir: false,
   build: {
     lib: {
-      entry: resolve(__dirname, 'src/main.js'),
+      entry: resolve(import.meta.dirname, 'src/main.js'),
       name: 'dynamicImport',
       formats: ['es', 'cjs', 'umd'],
       fileName: (format) => `dynamic-import.${format === 'es' ? 'esm.mjs' : format === 'cjs' ? 'cjs' : 'umd.js'}`
